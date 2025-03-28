8741385c95ed8a8ac06659302562c3d43533162e
raymund-corpuz <raymundcorpuz86@gmail.com>
2025-03-28 20:22:17 +0800

    usePosts

--- src/PostContext.js
+++ src/PostContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 import { faker } from "@faker-js/faker";
 
 function createRandomPost() {
@@ -49,4 +49,12 @@ function PostProvider({ children }) {
   );
 }
 
-export { PostProvider, PostContext };
+function usePosts() {
+  const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("PostContext was used outside of the PostProvider");
+
+  return context;
+}
+
+export { PostProvider, usePosts };
